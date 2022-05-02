66b70228c57d736893d44d22008d6da6c7d6e415
Thi Van An Truong <antr.2201@gmail.com>
2022-05-02 15:06:30 +0200

    feat: create room, list rooms, join room by id

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,42 @@
 <template>
-  <img alt="Vue logo" src="../assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="createRoom">Create Room</button>
+  <h1>Rooms</h1>
+  <button
+    v-for="(room, roomIndex) in rooms"
+    :key="roomIndex"
+    @click="joinRoom(room.roomId)"
+  >
+    Join Room {{ room.roomId }}
+  </button>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld";
+import {
+  client,
+  getRooms,
+  createRoom as create,
+  joinRoom as join,
+} from "../colyseus";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  data() {
+    return {
+      client: null,
+      rooms: [],
+    };
+  },
+  mounted() {
+    this.client = client();
+  },
+  methods: {
+    async createRoom() {
+      create(this.client, "my_room");
+      this.rooms = await getRooms(this.client, "my_room");
+    },
+    joinRoom(roomId) {
+      join(this.client, roomId);
+    },
+  },
+};
 </script>
