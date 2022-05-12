9e6aee43805aa92cbc7877ae3995c28be2bd1839
MorganeSauneuf <morgane.sauneuf@gmail.com>
2022-05-12 16:20:32 +0200

    feat: create EndGame page

---
+++ src/views/EndGameView.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Fin du jeu</h1>
+  <router-link to="/">back to home</router-link>
+</template>
+
+<script>
+import useColyseusStore from "@/store/colyseus";
+
+export default {
+  name: "EndGameView",
+  setup() {
+    const colyseus = useColyseusStore();
+
+    return { colyseus };
+  },
+  mounted() {
+    console.log(this.colyseus.currentRoom);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
