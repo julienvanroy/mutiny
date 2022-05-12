c732fc5885733790203e67e0b6420524fb83e589
MorganeSauneuf <morgane.sauneuf@gmail.com>
2022-05-12 16:20:07 +0200

    feat: create Rules/Parameters page

---
+++ src/views/RulesView.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>paramètres & règles du jeu</h1>
+  <router-link to="/game#debug">c'est partiiiii</router-link>
+</template>
+
+<script>
+import useColyseusStore from "@/store/colyseus";
+
+export default {
+  name: "RulesView",
+  setup() {
+    const colyseus = useColyseusStore();
+
+    return { colyseus };
+  },
+  mounted() {
+    console.log(this.colyseus.currentRoom);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  display: block;
+  margin: 8px auto;
+}
+</style>
