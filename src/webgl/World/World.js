63ed3f2039002f35b5e5c4863ddd66e7420fb1e2
Julien Vanroy <julien.vanroy@akqa.com>
2022-05-15 19:18:59 +0200

    feat: debug player keyboard

--- src/webgl/World/World.js
+++ src/webgl/World/World.js
@@ -2,7 +2,7 @@ import Environment from "./Environment.js";
 import Prison from "./Prison.js";
 import {component} from "bidello";
 import Experience from "@/webgl/Experience";
-import {GridHelper} from "three";
+import {GridHelper, Vector2} from "three";
 import Player from "@/webgl/World/Player";
 import Item from "@/webgl/World/Item";
 import BoxCollision from "@/webgl/Collision/BoxCollision";
@@ -10,9 +10,12 @@ import BoxCollision from "@/webgl/Collision/BoxCollision";
 export default class World extends component() {
     init() {
         const experience = new Experience();
+        this._debug = experience.debug
         this._renderer = experience.renderer;
         this._scene = experience.scene;
         this._camera = experience.camera;
+        this._controls = experience.controls;
+
         this._isLoaded = false;
     }
 
@@ -20,32 +23,76 @@ export default class World extends component() {
         console.log("world is ready");
         this.prison = new Prison();
         this.environment = new Environment();
-        this.players = []
+        this.players = new Map()
         console.log(typeof this.players)
         this.item = new Item();
         this.boxCollision = new BoxCollision()
         const grid = new GridHelper(20, 20);
         this._scene.add(grid);
+        this.onDebug()
         this._isLoaded = true;
+    }
+
+    _keyboard() {
+        const player = this.players.get('debug')
+        if (!this._debug.active || !this._controls.isPressed || !player) return
+
+        const vectorControls = new Vector2()
+
+        if (this._controls.actions.up && this._controls.actions.down)
+            vectorControls.y = 0;
+        else if (this._controls.actions.up)
+            vectorControls.y = 1;
+        else if (this._controls.actions.down)
+            vectorControls.y = -1;
+        else vectorControls.y = 0;
+
+        if (this._controls.actions.right && this._controls.actions.left)
+            vectorControls.x = 0;
+        else if (this._controls.actions.right)
+            vectorControls.x = 1;
+        else if (this._controls.actions.left)
+            vectorControls.x = -1;
+        else vectorControls.x = 0;
+
+        player.vectorControls = vectorControls
     }
 
     onRaf() {
         this._renderer.render(this._scene, this._camera);
 
-        /*
         if (this._isLoaded) {
-            this.players.map(player => {
-                console.log(player.id, this.boxCollision.hit(player.mesh, this.item.mesh))
+            this._keyboard()
+
+            // Check Collision Items
+            this.players.forEach((player, id) => {
+                if(this.boxCollision.hit(player.mesh, this.item.mesh)) console.log(id, 'Collision')
             })
         }
-        */
     }
 
     onAddPlayer({playerId}) {
-        this.players[playerId] = new Player(playerId);
+        this.players.set(playerId, new Player());
+        console.log(this.players, typeof this.players)
     }
 
     onMovePlayer({playerId, vector2}) {
-        this.players[playerId].vectorControls = vector2
+        const player = this.players.get(playerId)
+        player.vectorControls = vector2
+    }
+
+    onDebug() {
+        if (!this._debug.active) return;
+
+        // TweakPane
+        const folderDebug = this._debug.pane.addFolder({
+            title: "World",
+            expanded: false,
+        });
+
+        const btnAddPlayer = folderDebug.addButton({
+            title: 'addPlayer',
+        });
+        btnAddPlayer.on('click', () => this.onAddPlayer({playerId: 'debug'}));
     }
 }
