369b03753d70b67b718ec1ac63b5b6dcabd1ec77
Julien Vanroy <julien.vanroy@akqa.com>
2022-05-15 19:18:59 +0200

    feat: debug player keyboard

--- src/webgl/World/Player.js
+++ src/webgl/World/Player.js
@@ -6,25 +6,16 @@ import {
   Vector3,
 } from "three";
 import Experience from "../Experience";
-import EnvironmentCollision from "@/webgl/World/EnvironmentCollision";
 
 export default class Player extends component() {
-  constructor(playerId, playerName) {
-    super();
-    this.id = playerId;
-    this.name = playerName;
-  }
-
   init() {
     const experience = new Experience();
     this._scene = experience.scene;
     this.resource = experience.resources.items.robotModel
-    this._controls = experience.controls;
 
     this._vectorControls = new Vector2();
     this._targetQuaternion = new Quaternion();
     this._speedRotation = 10;
-    this.playerSpeed = 6.4;
 
     this._initModel();
   }
@@ -39,38 +30,15 @@ export default class Player extends component() {
         child.castShadow = true
       }
     })
-
-    this.collision = {
-      environment: new EnvironmentCollision(this),
-    };
   }
 
   set vectorControls(value) {
-    this._vectorControls = new Vector2(value.x, value.y)
+    this._vectorControls.x = value.x;
+    this._vectorControls.y = value.y
   }
 
   get isMoving() {
     return this._vectorControls.x !== 0 || this._vectorControls.y !== 0 ;
-  }
-
-  _keyboard() {
-    if (!this._controls.isPressed) return
-
-    if (this._controls.actions.up && this._controls.actions.down)
-      this._vectorControls.y = 0;
-    else if (this._controls.actions.up)
-      this._vectorControls.y = this.collision.params.playerSpeed;
-    else if (this._controls.actions.down)
-      this._vectorControls.y = -this.collision.params.playerSpeed;
-    else this._vectorControls.y = 0;
-
-    if (this._controls.actions.right && this._controls.actions.left)
-      this._vectorControls.x = 0;
-    else if (this._controls.actions.right)
-      this._vectorControls.x = this.playerSpeed;
-    else if (this._controls.actions.left)
-      this._vectorControls.x = -this.playerSpeed;
-    else this._vectorControls.x = 0;
   }
 
   _move(delta) {
@@ -94,7 +62,6 @@ export default class Player extends component() {
   }
 
   onRaf({ delta }) {
-    this._keyboard();
     this._move(delta);
     this._rotation(delta);
   }
