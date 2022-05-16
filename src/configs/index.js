110fb510315ea61f3f238ed849738426c38ed846
Thi Van An Truong <antr.2201@gmail.com>
2022-05-16 23:31:18 +0200

    fix: change color palette and material

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,7 +1,7 @@
 const configs = {
     tempCharacter: {
         count: 64,
-        colors: ["#365e55", "#72341f", "#c79161", "#c4ac90", "#dbcdc1"],
+        colors: ["#414182", "#8DDDE9", "#FCA04C", "#4F9C98", "#FC6C4C"],
         sizes: {
             radius: 0.32,
             height: 0.64,
--- src/webgl/World/Mover.js
+++ src/webgl/World/Mover.js
@@ -1,20 +1,20 @@
-import {ConeGeometry, Group, Mesh, MeshBasicMaterial, Color, BoxGeometry, CylinderGeometry} from "three";
+import { ConeGeometry, Group, Mesh, MeshStandardMaterial, Color, BoxGeometry, CylinderGeometry } from "three";
 import Experience from "../Experience";
 import configs from "@/configs";
 import { sampleSize, shuffle } from "@/utils";
 const { colors, sizes } = configs.tempCharacter;
 
 export default class Mover {
-  constructor() {
-      const experience = new Experience();
-      this._scene = experience.scene;
-      //this.resource = experience.resources.items.robotModel;
-      //this._initModel();
+    constructor() {
+        const experience = new Experience();
+        this._scene = experience.scene;
+        //this.resource = experience.resources.items.robotModel;
+        //this._initModel();
 
-      this._initMesh();
-  }
+        this._initMesh();
+    }
 
-  /*
+    /*
   _initModel() {
     this.mesh = this.resource.scene.clone();
     this.mesh.scale.set(0.08, 0.08, 0.08);
@@ -34,26 +34,22 @@ export default class Mover {
         const c = sampleSize(colors, 3);
 
         const cube = new Mesh(
-            new BoxGeometry(
-                Math.random() > 0.5 ? r : h,
-                h,
-                Math.random() > 0.5 ? r : h
-            ),
-            new MeshBasicMaterial({ color: new Color(c[0]).convertSRGBToLinear() })
+            new BoxGeometry(Math.random() > 0.5 ? r : h, h, Math.random() > 0.5 ? r : h),
+            new MeshStandardMaterial({ color: new Color(c[0]).convertSRGBToLinear() })
         );
         cube.tags = ["Cube"];
         cube.color = c[0];
 
         const cylinder = new Mesh(
             new CylinderGeometry(r, r, h, 32),
-            new MeshBasicMaterial({ color: new Color(c[1]).convertSRGBToLinear() })
+            new MeshStandardMaterial({ color: new Color(c[1]).convertSRGBToLinear() })
         );
         cylinder.tags = ["Cylinder"];
         cylinder.color = c[1];
 
         const cone = new Mesh(
             new ConeGeometry(r, h, 32),
-            new MeshBasicMaterial({ color: new Color(c[2]).convertSRGBToLinear() })
+            new MeshStandardMaterial({ color: new Color(c[2]).convertSRGBToLinear() })
         );
         cone.tags = ["cone"];
         cone.color = c[2];
