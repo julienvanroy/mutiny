bf0d2dd13225dfd9b3937bb7ffadd98c59c68b5b
Thi Van An Truong <antr.2201@gmail.com>
2022-05-17 12:41:36 +0200

    fix: adjust bots count

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,6 @@
 const configs = {
     character: {
-        count: 4, //4
+        count: 32, //4
         colors: ["#414182", "#8DDDE9", "#FCA04C", "#4F9C98", "#FC6C4C"],
         sizes: {
             radius: 0.32,
