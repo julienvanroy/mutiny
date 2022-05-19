d6714cc20c876c0041c16d629554ba2b3ddf03ac
Thi Van An Truong <antr.2201@gmail.com>
2022-05-19 12:03:36 +0200

    feat: mecanica A kills B, leading to A targets C, B respawn and targets A

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,6 @@
 const configs = {
     character: {
-        count: 32, //4
+        count: 8, //4
         colors: ["#414182", "#8DDDE9", "#FCA04C", "#4F9C98", "#FC6C4C"],
         sizes: {
             radius: 0.32,
@@ -11,7 +11,7 @@ const configs = {
         range: 3.2, //10
     },
     game: {
-        maxTime: 150,
+        maxTime: 600, //150,
     },
 };
 
