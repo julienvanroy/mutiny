01ef152388c1c7bdb67454c2c3b3b25e2aed1ede
Thi Van An Truong <antr.2201@gmail.com>
2022-05-20 16:28:15 +0200

    fix: remettre prod configs

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,12 +1,12 @@
 const configs = {
     character: {
-        count: 8, //4
+        count: 32, //4
         colors: ["#414182", "#8DDDE9", "#FCA04C", "#4F9C98", "#FC6C4C"],
         sizes: {
             radius: 0.32,
             height: 0.64,
         },
-        speed: 3.2,
+        speed: 1,
         rotation: 100,
         range: 3.2, //10
     },
