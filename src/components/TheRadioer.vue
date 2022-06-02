8dd3626d217517e20170f03fb3f71075644b1763
MorganeSauneuf <morgane.sauneuf@gmail.com>
2022-06-02 14:18:33 +0200

    feat: add OptionContainer and TheRadioer components

---
+++ src/components/TheRadioer.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="radioer">
+    <p class="subtitle" v-if="!!subtitle">{{ subtitle }}</p>
+    <div class="radios">
+      <div class="radio" v-for="(value, idx) in values" :key="`val-${idx}`">
+        <input
+          type="radio"
+          :id="value"
+          :name="label"
+          :value="value"
+          :defaultChecked="defaultValue === value"
+        />
+        <label :for="value">{{ value }}</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheRadioer",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+    },
+    values: {
+      type: Array,
+      required: true,
+    },
+    defaultValue: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.radioer {
+  .subtitle {
+    font-size: $ft-s-small;
+  }
+  .radios {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 20px;
+    .radio {
+      position: relative;
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: center;
+      align-items: center;
+      input[type="radio"] {
+        position: relative;
+        &:before {
+          content: "";
+          display: block;
+          width: 21px;
+          height: 24px;
+          background-color: #fff3f2;
+          background-image: url("../assets/parameters/radio.png");
+          background-repeat: no-repeat;
+          background-size: 100% 100%;
+          transform: translate(-30%, -40%);
+          transform-origin: center;
+          transition: .3s all ease-in-out;
+        }
+        @media (any-hover: hover) {
+          cursor: pointer;
+        }
+        &:checked {
+          &:before {
+            width: 32px;
+            height: 36px;
+            background-image: url("../assets/parameters/radio-checked.png");
+            transition: .3s all ease-in-out;
+          }
+        }
+      }
+      label {
+        font-size: $ft-s-xsmall;
+        margin-top: 10px;
+        @media (any-hover: hover) {
+          cursor: pointer;
+        }
+      }
+      & + .radio {
+        margin-left: 60px;
+        input[type="radio"] {
+          &:after {
+            content: "";
+            width: 74px;
+            height: 2px;
+            background-color: #dec5cc;
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform: translateX(-116%);
+          }
+        }
+      }
+    }
+  }
+}
+</style>
