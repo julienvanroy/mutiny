01c921b4cbdd96b2403756fc0d8801390abd3d00
Thi Van An Truong <antr.2201@gmail.com>
2022-06-07 06:51:06 +0200

    feat: waiting view

---
+++ src/components/ui/PlayerCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="player-card">
+    <img :src="img" alt="" />
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlayerCard",
+  props: {
+    color: {
+      type: String,
+      required: true,
+      //add validator
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    large: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    img() {
+      switch (this.color) {
+        default:
+          return "/playerCard/purple.png";
+      }
+    },
+  },
+  mounted() {
+    console.log(this);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.player-card {
+  position: relative;
+  background-image: url("../../assets/playerCard/background.png");
+  background-repeat: no-repeat;
+  background-size: 90%;
+  background-position: 12px 12px;
+  overflow: visible;
+  width: 240px;
+  height: 111px;
+
+  p {
+  }
+}
+</style>
--- src/views/WaitingView.vue
+++ src/views/WaitingView.vue
@@ -1,26 +1,111 @@
 <template>
   <div class="waiting">
-    <header class="get-pseudo__header">
-      <img src="../assets/mobile/header-w-logo.svg" alt="" />
-    </header>
-    <h2>{{ $t("getPseudo.title") }}</h2>
-    <the-button @click="$router.push('gamepad')" />
-    <footer class="get-pseudo__header">
-      <img src="../assets/mobile/header.svg" alt="" />
-    </footer>
+    <template v-if="!isLandscape">
+      <header>
+        <img src="../assets/mobile/header-w-logo.svg" alt="" />
+      </header>
+      <h2>{{ $t("waiting.portrait.title") }}</h2>
+      <player-card :name="colyseus.playerName" :color="colyseus.playerColor" large />
+      <div class="waiting__instruction">
+        <p>{{ $t("waiting.portrait.instruction") }}</p>
+        <img src="../assets/mobile/icon-rotate.svg" alt="" />
+      </div>
+      <footer>
+        <img src="../assets/mobile/header.svg" alt="" />
+      </footer>
+    </template>
+    <template v-else>
+      <header>
+        <img src="../assets/mobile/header.svg" alt="" />
+      </header>
+      <h2>
+        {{ $t("waiting.landscape.titleLeft") }}
+        <em>{{ $t("waiting.landscape.titleMiddle") }}</em>
+        {{ $t("waiting.landscape.titleRight") }}
+      </h2>
+      <p>{{ $t("waiting.landscape.instruction") }}</p>
+      <footer>
+        <img src="../assets/mobile/header.svg" alt="" />
+      </footer>
+    </template>
   </div>
 </template>
 
 <script>
-import TheButton from "@/components/ui/TheButton.vue";
+import useColyseusStore from "@/store/colyseus";
+import { mapWritableState } from "pinia";
+import useGlobalStore from "@/store/global";
+import PlayerCard from "@/components/ui/PlayerCard.vue";
 
 export default {
-  components: { TheButton },
+  components: { PlayerCard },
   name: "WaitingView",
+  setup() {
+    const colyseus = useColyseusStore();
+    return { colyseus };
+  },
+  computed: {
+    ...mapWritableState(useGlobalStore, ["isLandscape"]),
+  },
+  mounted() {
+    this.resize();
+    window.addEventListener("resize", this.resize, false);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.resize, false);
+  },
+  methods: {
+    resize() {
+      this.isLandscape = window.innerWidth > window.innerHeight;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .waiting {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background-color: $mobile-bg-white;
+  background-image: url("../assets/mobile/background.png");
+  background-size: contain;
+  background-position: center;
+  background-repeat: repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 32px;
+  text-align: center;
+
+  @media (orientation: landscape) {
+    padding-left: 24%;
+    padding-right: 24%;
+    font-size: 20px;
+    line-height: 25px;
+    font-weight: $ft-w-bold;
+
+    h2 em {
+      color: $blue;
+    }
+  }
+
+  @media (orientation: portrait) {
+    h2 {
+      font-weight: $ft-w-bold;
+      font-size: 30px;
+      text-align: center;
+    }
+
+    &__instruction {
+      p {
+        padding: 0 16%;
+        margin-bottom: 32px;
+        font-size: 14px;
+        letter-spacing: 0.12em;
+      }
+    }
+  }
 }
 </style>
