f52c5b4c4cf17fc33e17b75f9c9d31a43a1c686e
Julien Vanroy <julien.vanroy@akqa.com>
2022-06-14 19:13:07 +0200

    feat: material sail

---
+++ src/webgl/Materials/SailMaterial.js
@@ -0,0 +1,19 @@
+import {ShaderMaterial, Vector2} from "three";
+import vertexShader from '@/shaders/sail/sail.vert'
+import fragmentShader from '@/shaders/sail/sail.frag'
+import * as THREE from "three";
+
+export class SailMaterial extends ShaderMaterial {
+    constructor() {
+        super({
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader,
+            uniforms:
+                {
+                    uColor: { value: new THREE.Color('#ffffff') },
+                    uFrequency: {value: new Vector2(1, 1)},
+                    uTime: {value: 0},
+                }
+        });
+    }
+}
