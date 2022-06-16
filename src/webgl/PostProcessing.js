e68a05c5897f006b4312c23e786e7ff077b35b9c
Julien Vanroy <julien.vanroy@akqa.com>
2022-06-17 00:12:30 +0200

    feat: add all options

--- src/webgl/PostProcessing.js
+++ src/webgl/PostProcessing.js
@@ -1,15 +1,16 @@
 import {
-    BloomEffect,
+    BloomEffect, ChromaticAberrationEffect,
     EffectComposer,
     EffectPass,
-    RenderPass,
+    RenderPass, ShockWaveEffect,
     SMAAEffect,
     SMAAPreset,
     ToneMappingEffect,
-    ToneMappingMode
+    ToneMappingMode, VignetteEffect,
 } from "postprocessing";
 import {component} from "bidello";
 import Experience from "@/webgl/Experience";
+// import {Vector3} from "three";
 
 export default class PostProcessing extends component() {
     init() {
@@ -44,20 +45,47 @@ export default class PostProcessing extends component() {
         this._scene = experience.scene
         this._camera = experience.camera
         this.renderPass = new RenderPass(this._scene, this._camera)
+
         const smaaEffect = new SMAAEffect({preset: this._params.smaa.preset.medium})
         const bloomEffect = new BloomEffect({intensity: this._params.bloom.intensity})
         const toonEffect = new ToneMappingEffect({mode: this._params.tone.mode.ACES_FILMIC})
+
+        // Drunk
+        const chromaEffect = new ChromaticAberrationEffect()
+        const vignetteEffect = new VignetteEffect({darkness: 0.65})
+
         this.effectPass = new EffectPass(this._camera, smaaEffect, bloomEffect, toonEffect);
-        this.effectComposer = new EffectComposer(this._renderer)
+        this.effectComposer = new EffectComposer(this._renderer);
 
         this.effectComposer.addPass(this.renderPass);
         this.effectComposer.addPass(this.effectPass);
+        this.effectComposer.addPass(new EffectPass(this._camera, chromaEffect, vignetteEffect))
+        this.effectComposer.addPass(new EffectPass(this._camera, new ShockWaveEffect(this._camera)))
 
         this.smaaEffect = this.effectComposer.passes[1].effects[0]
         this.bloomEffect = this.effectComposer.passes[1].effects[1]
         this.toneEffect = this.effectComposer.passes[1].effects[2]
 
+        // Drunk
+        this.chromaEffect = this.effectComposer.passes[2].effects[0]
+        this.vignetteEffect = this.effectComposer.passes[2].effects[1]
+        this.shockWaveEffect =  this.effectComposer.passes[3].effects[0]
+
+        /*
+        this.shockWaveEffect.waveSize  = 5
+        this.shockWaveEffect.speed  = 1
+        this.shockWaveEffect.size  = 100
+        this.shockWaveEffect.entent  = 0.9
+        this.shockWaveEffect.amplitude  = 0.4
+        setInterval(this.explodeDrunk,6000)
+         */
+
         this.onDebug()
+    }
+
+    explodeDrunk() {
+        // this.shockWaveEffect.position = new Vector3(Math.random() * 20, Math.random() * 20, Math.random() * 20)
+        this.shockWaveEffect.explode()
     }
 
     onDebug() {
@@ -91,7 +119,7 @@ export default class PostProcessing extends component() {
             title: "Bloom",
             expanded: false,
         });
-        folderBloom.addInput(this.bloomEffect.luminanceMaterial, 'threshold',{
+        folderBloom.addInput(this.bloomEffect.luminanceMaterial, 'threshold', {
             label: "threshold",
             step: 0.001,
             min: 0,
@@ -154,6 +182,68 @@ export default class PostProcessing extends component() {
             min: 0,
             max: 1,
         })
+
+        const folderDrunk = folderDebug.addFolder({
+            title: "Drunk Effect",
+            expanded: false,
+        });
+        const folderChroma = folderDrunk.addFolder({
+            title: "Chroma Effect",
+            expanded: false,
+        });
+        folderChroma.addInput(this.chromaEffect, 'offset', {
+            label: "offset",
+        })
+        const folderVignette = folderDrunk.addFolder({
+            title: "Vignette Effect",
+            expanded: false,
+        });
+        folderVignette.addInput(this.vignetteEffect, 'darkness', {
+            label: "darkness",
+            step: 0.001,
+            min: 0,
+            max: 3,
+        })
+        folderVignette.addInput(this.vignetteEffect, 'offset', {
+            label: "offset",
+        })
+        const folderWave = folderDrunk.addFolder({
+            title: "Shock Wave Effect",
+            expanded: false,
+        });
+        folderWave.addInput(this.shockWaveEffect, 'position', {
+            label: "position",
+        })
+        folderWave.addInput(this.shockWaveEffect, 'epicenter', {
+            label: "epicenter"
+        })
+        folderWave.addInput(this.shockWaveEffect, 'speed', {
+            label: "speed",
+            step: 0.001,
+            min: 0,
+            max: 10,
+        })
+        folderWave.addInput(this.shockWaveEffect, 'maxRadius', {
+            label: "maxRadius",
+            step: 0.001,
+            min: 0,
+            max: 20,
+        })
+        folderWave.addInput(this.shockWaveEffect, 'waveSize', {
+            label: "waveSize",
+            step: 0.001,
+            min: 0,
+            max: 10,
+        })
+        folderWave.addInput(this.shockWaveEffect, 'amplitude', {
+            label: "amplitude",
+            step: 0.001,
+            min: 0,
+            max: 1,
+        })
+        folderWave.addButton({title: "Explode"}).on("click", () => {
+            this.explodeDrunk()
+        });
     }
 
     onRaf() {
